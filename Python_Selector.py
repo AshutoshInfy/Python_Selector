614c2f4d2ae84b29714b77e5b8c9d87231d6bf07
Zachary Smith <zacharysmith2000@googlemail.com>
2017-10-08 20:50:07 +0100

    Update Python_Selector.py

--- Python_Selector.py
+++ Python_Selector.py
@@ -1,31 +1,12 @@
-#Python Selector Program BETA
-#Created by Lewis Watson (aka Wo)
-
-def createlist():
-	user_list = []
-	print("")
-	loops = int(input("How many items do you wish to select from? "));
-	print("")
-
-	if loops <1:
-		print("Please select a valid number of list items.")
-		createlist();
-
-	else:
-		while loops != 0:
-			loops = loops - 1;
-			user_input = input("Select Item: ");
-			user_list = user_list + [user_input];
-
-		print("You have selected " + str(len(user_list)) + " items.")
-		print("");
-		selector(user_list);
-
-def selector(user_list):
-	print(user_list)
-	print("END OF PROGRAM")
-
-
-  
-
-createlist();
+def create_list():    #  defines the create_list function
+	
+	#  1. takes a string input of items to add to the list, e.g.: "1,2,3,4,5"
+	#  2. then splits the string at the character ',' turning it into a list
+	#  3. finally returns the list to the user
+	
+	return input("Enter list items, seperated by a ','\n > ").split(",")
+	
+if __name__ == "__main__":    #  when the program is run
+	new_list = create_list()    #  create a new list
+	print(new_list)    #  print the newly created list
+	
